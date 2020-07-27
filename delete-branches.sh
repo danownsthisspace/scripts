f7be4a0c5ac8a641a334418397c1835ac8e74cce
Daniel Amber <daniel.amber@icloud.com>
2020-07-27 09:02:55 +0200

    Initial commit

---
+++ delete-branches.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for branch in $(git for-each-ref --format='%(refname:short)' refs/heads/ | grep "$1") ; do
+    read -p "Do you want to delete: $branch? (y/n): " answer
+    if [ "$answer" = "y" ]
+    then
+        git branch -D "$branch"
+    else
+        echo "skipping"
+    fi
+done
